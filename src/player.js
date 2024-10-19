06499737dd5cf47b5d624ce3ab63ae2533c18584
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-10-19 14:28:20 +0200

    add player class with some attributes and methods

--- src/player.js
+++ src/player.js
@@ -1,2 +1,16 @@
 console.log('PLAYER IS WORKING')
 
+class Player {
+    static playerArray = []
+    constructor() {
+        this.life = 3;
+    }
+
+    addChosenElement(choice){
+        Player.playerArray.push(choice)
+    }
+
+    // displayChoices(){
+    // }
+}
+}
