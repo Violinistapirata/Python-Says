690e8e8a8fe0c3b0976f444de3f342af757286e2
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-10-23 20:36:42 +0200

    add functionality to reproduce the pytonArray

--- src/python.js
+++ src/python.js
@@ -1,16 +1,39 @@
-const python = {
-  
+const python = {};
 
-}
-
-const pythonArray = [];
+const pythonArray = [1, 2, 3, 4];
 
 function generateRandomNumber() {
-    let randomNumber = Math.ceil(Math.random()*4);
-    return randomNumber;
+  let randomNumber = Math.ceil(Math.random() * 4);
+  return randomNumber;
 }
 let randomNumber = 0;
-setInterval(()=>{
+function displayPythonArray() {
+  let counter = 0;
+  const intervalId = setInterval(() => {
+    switch (pythonArray[counter]) {
+      case 1:
+        htmlButton.highlight(htmlButton.highlightColor);
+        break;
+      case 2:
+        cssButton.highlight(cssButton.highlightColor);
+        break;
+      case 3:
+        nodeButton.highlight(nodeButton.highlightColor);
+        break;
+      case 4:
+        jsButton.highlight(jsButton.highlightColor);
+        break;
+      default:
+        break;
+    }
+    counter++;
+    if (counter == pythonArray.length) clearInterval(intervalId);
+  }, 1000);
+}
+
+displayPythonArray();
+
+/* setInterval(()=>{
     randomNumber = generateRandomNumber();
     console.log(randomNumber);
     
@@ -30,8 +53,8 @@ setInterval(()=>{
         default:
             break;
     }
-}, 1000);
+}, 1000); */
 
 generateRandomNumber();
 
-console.log(generateRandomNumber())
+console.log(generateRandomNumber());
