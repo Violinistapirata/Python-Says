948bf21481d4a4b6ea016017edd2b3776a657103
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-11-01 17:18:59 +0100

    commit before getting rid of useless HTML refferences

--- src/logic.js
+++ src/logic.js
@@ -1,27 +1,26 @@
 // HTML ELEMENTS
 
-// const gameArea = document.querySelector('#game-area');
-const navBar = document.querySelector("#nav-bar");
-const lives = document.querySelector("#lives");
-const menuIcon = document.querySelector("#menu-icon");
+// const navBar = document.querySelector("#nav-bar");
+// const lives = document.querySelector("#lives");
+// const menuIcon = document.querySelector("#menu-icon");
+// const biteImgElement = document.querySelector("#bite");
+// const instructions1 = document.querySelector("#instructions1");
+// const instructions2 = document.querySelector("#instructions2");
+// const backgroundMusic = document.getElementById("backgroundMusic");
 
-const answerLine = document.querySelector("#verification");
 
-const biteImgElement = document.querySelector("#bite");
-const menuButton = document.querySelector("#box-container-menu");
-const gameOverPrompt = document.querySelector("#box-container-game-over");
-const victoryPrompt = document.querySelector("#box-container-victory");
-const arrowSign = document.querySelector("#arrow");
-const instructions1 = document.querySelector("#instructions1");
-const instructions2 = document.querySelector("#instructions2");
-const muteButton = document.querySelector("#mute");
-const backgroundMusic = document.getElementById("backgroundMusic");
+
 
 const playerArray = [];
+
+
 
 // ARRAY TO BE DISPLAYED ON SCREEN
 
 const answersArray = [];
+
+
+
 
 // TURN BACKGROUND MUSIC ON/OFF
 
@@ -45,6 +44,12 @@ function toggleMusic() {
 muteButton.addEventListener("click", toggleMusic);
 
 stopMusic();
+
+
+
+
+
+
 
 // This function appends to the answerLine all the icon elements in the answersArray
 function displayAnswersArray() {
---
+++ src/refferences.js
@@ -0,0 +1,6 @@
+const answerLine = document.querySelector("#verification");
+const menuButton = document.querySelector("#box-container-menu");
+const gameOverPrompt = document.querySelector("#box-container-game-over");
+const victoryPrompt = document.querySelector("#box-container-victory");
+const arrowSign = document.querySelector("#arrow");
+const muteButton = document.querySelector("#mute");
